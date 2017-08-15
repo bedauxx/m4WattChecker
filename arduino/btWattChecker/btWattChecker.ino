fb97605c2b80a8e475c340fe05a6b6966005fbe7
bedauxx <bedauxx@gmail.com>
2017-08-15 12:04:11 +0900

    不要記述削除

--- arduino/btWattChecker/btWattChecker.ino
+++ arduino/btWattChecker/btWattChecker.ino
@@ -13,15 +13,11 @@ void setup() {
 
 void loop() {
 
-  float shuntvoltage = 0;
   float busvoltage = 0;
   float current_mA = 0;
-  float loadvoltage = 0;
 
- // shuntvoltage = ina219.getShuntVoltage_mV();
   busvoltage = ina219.getBusVoltage_V();
   current_mA = ina219.getCurrent_mA() /1000 ;
- // loadvoltage = busvoltage + (shuntvoltage / 1000);
     
     if(busvoltage==0){
          delay(100);
