9d9632bbf6081446a2a24b0f0e50e8b66bcb80da
bedauxx <bedauxx@gmail.com>
2017-08-20 23:14:53 +0900

    修正と新バージョン追加

---
+++ arduino/m4WattChecker/m4WattChecker.ino
@@ -0,0 +1,38 @@
+#include <Thanos_INA260.h>
+
+
+
+//#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+Thanos_INA260 ina260;
+
+
+void setup() {
+    Serial.begin(115200);
+    ina260.begin();
+}
+
+void loop() {
+
+  float busvoltage = 0;
+  float current_mA = 0;
+
+  busvoltage = ina260.getBusVoltage_V();
+  current_mA = ina260.getCurrent_mA() /1000 ;
+    
+    if(busvoltage==0){
+         delay(100);
+    }else if(busvoltage>5){
+         delay(100);
+    }else{
+         Serial.print(busvoltage);
+         Serial.print("V,");
+         Serial.print(current_mA);
+         Serial.println("A");
+         delay(250);
+    }
+
+}
+
+
